4f4a5c2eac60b552565c263b2d7fe0996bba0d18
paulinamonstaviciute <paulinamonstaviciute@gmail.com>
2019-12-12 21:15:19 +0200

    padaryta uzduotis operatoriai_pirmas

--- operatoriai_pirmas/operatoriai_pirmas/Program.cs
+++ operatoriai_pirmas/operatoriai_pirmas/Program.cs
@@ -10,6 +10,11 @@ namespace operatoriai_pirmas
     {
         static void Main(string[] args)
         {
+            int skaicius;
+            Console.WriteLine("Iveskite skaiciu");
+            skaicius = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}{0}{0}{0}", skaicius);
+            Console.ReadLine();
         }
     }
 }
