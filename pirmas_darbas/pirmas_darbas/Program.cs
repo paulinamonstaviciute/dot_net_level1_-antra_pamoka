5f9b73ad27b11338f99af135d10b770b1db4efd6
paulinamonstaviciute <paulinamonstaviciute@gmail.com>
2019-12-12 18:48:23 +0200

    parasem programa

--- pirmas_darbas/pirmas_darbas/Program.cs
+++ pirmas_darbas/pirmas_darbas/Program.cs
@@ -10,6 +10,22 @@ namespace pirmas_darbas
     {
         static void Main(string[] args)
         {
+            int yearOfBirth;
+            string firstName;
+
+            var lastName = "Pavardenis";
+            decimal salary = 1999.9m;
+            DateTime lastPayDay = DateTime.Today.AddMonths(-1);
+
+            yearOfBirth = 1990;
+            firstName = "Vardenis";
+
+            Console.WriteLine("Vardas, Pavarde: {0}, {1}", firstName, lastName);
+            Console.WriteLine("Gimimo metai: {0}", yearOfBirth);
+            Console.WriteLine("Pajamos: {0}", salary);
+            Console.WriteLine("Paskutini karta ismoketa alga: {0}", lastPayDay.ToLongDateString());
+
+            Console.ReadLine();
         }
     }
 }
