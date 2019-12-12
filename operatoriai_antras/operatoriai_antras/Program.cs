4623ded62aed06a55dbfd00e952338db0c53002a
paulinamonstaviciute <paulinamonstaviciute@gmail.com>
2019-12-12 21:44:11 +0200

    padaryta uzduotis operatoriai_antras

--- operatoriai_antras/operatoriai_antras/Program.cs
+++ operatoriai_antras/operatoriai_antras/Program.cs
@@ -10,6 +10,14 @@ namespace operatoriai_antras
     {
         static void Main(string[] args)
         {
+            int a = 1;
+            int b = 15;
+            Console.WriteLine("Pries keitima: a = {0}, b = {1}", a, b);
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.WriteLine("po keitimo: a = {0}, b = {1}", a,b);
+            Console.ReadLine();
         }
     }
 }
