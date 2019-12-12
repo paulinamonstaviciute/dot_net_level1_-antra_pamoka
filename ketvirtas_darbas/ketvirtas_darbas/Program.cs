c43a0fbae477d62cb025195dc46d4a883135c19e
paulinamonstaviciute <paulinamonstaviciute@gmail.com>
2019-12-12 19:52:06 +0200

    padarytas ketvirtas_darbas

--- ketvirtas_darbas/ketvirtas_darbas/Program.cs
+++ ketvirtas_darbas/ketvirtas_darbas/Program.cs
@@ -10,6 +10,15 @@ namespace ketvirtas_darbas
     {
         static void Main(string[] args)
         {
+            string mokyklosPavadinimas = "CodeAcademy";
+            string kursoPavadinimas = ".NET";
+            int studentuSkaicius = 8;
+            DateTime siandienosData = DateTime.Today;
+            Console.WriteLine("Mokyklos pavadinimas: {0}", mokyklosPavadinimas);
+            Console.WriteLine("Kurso pavadinimas: {0}", kursoPavadinimas);
+            Console.WriteLine("Studentu skaicius: {0}", studentuSkaicius);
+            Console.WriteLine("Siandienos data: {0}", siandienosData);
+            Console.ReadLine();
         }
     }
 }
